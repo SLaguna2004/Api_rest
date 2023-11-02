1769966ca53841360897a0bdac584751ec962ced
Samuel Esteban Laguna Hernandez <samuh200413@gmail.com>
2023-11-02 06:50:51 -0500

    crecación de conexión y parametros

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ app.use(express.json());
     app.get('', function(req,res){
         res.send('');
     })
+
+//crear 
+//objeto de la conexion--  se establecen los parámetros
+let conexion = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'articulos'
+});
